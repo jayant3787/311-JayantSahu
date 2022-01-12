2b3f8eb3b73671dec42f9be8c715907044ea5667
jayant3787 <jayant3787@gmail.com>
2022-01-12 16:21:31 +0530

    pagination done

--- NodeJS_programs/online_shopping/src/controller/products.js
+++ NodeJS_programs/online_shopping/src/controller/products.js
@@ -1,9 +1,23 @@
 // import { fetchProducts } from "../services/products.cjs";
 import { fetchProducts } from "../services/products.js";
 const getProducts = ( req, res ) => {
-    const {sort,order, minPrice,minRating} = req.query;
-    console.log(sort,order, minPrice,minRating);
-    fetchProducts(sort,order,minPrice,minRating)
+    let {sort,order, minPrice,minRating,page} = req.query;
+    //convert a page to integer, and set a default of 1 or it is not passed
+    let pageInt = parseInt(page);
+
+    if(isNaN(page)){
+        pageInt = 1;
+    }
+
+    if(!sort){
+        sort = 'name';
+    }
+
+    if(!order){
+        order = 'asc';
+    }
+
+    fetchProducts(sort,order,pageInt,minPrice,minRating)
        .then(products => {
            res.json(products);
 
--- NodeJS_programs/online_shopping/src/services/products.js
+++ NodeJS_programs/online_shopping/src/services/products.js
@@ -2,24 +2,31 @@
  * Service talks to the database (data layer)
  */
 import mongoose from 'mongoose';
+import config from '../utils/config.js';
 
  const Product = mongoose.model( 'Product' );
  
- const fetchProducts = (sort,order, minPrice,minRating) => {
-     const filterClause = {
-        price:{
-            $gte:minPrice
-        },
-        rating:{
-            $gte:minRating
-        }
+ const fetchProducts = (sort,order,page, minPrice,minRating) => {
+     const skipClause = (page - 1) * config.PAGE_SIZE;
 
-    };
+     const filterClause = {};
+     if(minPrice){
+         filterClause.price ={
+            $gte:minPrice
+         };
+     }
+
+     if(minRating){
+        filterClause.rating ={
+           $gte:minRating
+        };
+    }
+     
 
     const sortClause = {
         [sort]:order === 'desc'? -1:1
     }
-     return Product.find(filterClause).sort(sortClause);
+     return Product.find(filterClause).sort(sortClause).skip(skipClause).limit(config.PAGE_SIZE);
  };
 export{
     fetchProducts
