e62f018f3f63a2b9c7728fc7cc448bf4d76d536e
jayant3787 <jayant3787@gmail.com>
2022-01-21 16:26:16 +0530

    changes done in shopping

--- NodeJS_programs/online_shopping/src/routes/users.js
+++ NodeJS_programs/online_shopping/src/routes/users.js
@@ -6,17 +6,17 @@ const router = express.Router();
 // Send a message when client requests for /users
 
 // for looged in user role 'admin'
-// router.get( '/',authenticate,authorize(['admin']), getUsers);
+router.get( '/',authenticate,authorize(['admin']), getUsers);
 
 
-// router.get( '/:_id',getUserById);
-// router.post( '/', postUser);
-// router.put( '/:_id', putUser);
-// router.delete('/:_id',deleteUser);
+router.get( '/:_id',getUserById);
+router.post( '/', postUser);
+router.put( '/:_id', putUser);
+router.delete('/:_id',deleteUser);
 
 // authorized for role 'admin'
 
-// router.get('/',authenticate,getUsers)
+router.get('/',authenticate,getUsers)
 
 
 export default router;
