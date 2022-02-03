02bece6f5a5927f0a24beb2c9b0f82bd7f83f63e
jayant3787 <jayant3787@gmail.com>
2022-02-03 12:59:22 +0530

    array exercise done

--- ReactJS_Programs/React_app/react_demo/src/components/functionalComponent_demo/GreetingES6.js
+++ ReactJS_Programs/React_app/react_demo/src/components/functionalComponent_demo/GreetingES6.js
@@ -10,10 +10,11 @@ const GreetingES6 = (props) => {
     // }
 
     // Hooks usage demo
+    //state of primitive numeric
     var initialAge = 24
 
     const [age, setAge] = useState(initialAge);
-    
+
     const changeAge = () => {
         if (age % 5 === 0)
             setAge(age + 5);
@@ -22,14 +23,28 @@ const GreetingES6 = (props) => {
     }
 
 
+    //state of string type variable
+    const myName = "Jeh"
+    const [ mName , setNm ] = useState(myName);
+
+    //state of object type variable
+
+    const [address, setAddress] = useState({doorNum:201, street:"1st cross road", road:"Dynasty layout, Mariyannanapalya", city:"Bangalore", state:"Karnataka"});
+
 
     return (
         <div>HELLO I AM  {props.name} and I AM {age}<br />
+        
 
             {/* 1st way */}
             <button onClick={() => setAge(age + 1)}>Change Age</button><br/>
             {/* 2nd way */}
-            <button onClick={changeAge}>Change Age by 5  if it is div by 5 otherwise 1 </button>
+            <button onClick={changeAge}>Change Age by 5  if it is div by 5 otherwise 1 </button><br/>
+            NAME: {mName}
+            <button onClick={() => setNm("Pooja")}>Change name</button><br/><br/>
+            
+            Address: {address.doorNum}, Street: {address.street}, Road: {address.road}, City: {address.city}, State: {address.state}<br/><br/>
+            <button onClick={() => setAddress({...address, doorNum:501})}>CHANGE DOOR NUMBER</button><br/><br/>
 
         </div>
     );
--- ReactJS_Programs/React_app/react_demo/src/components/functionalComponent_demo/PersonComponent.js
+++ ReactJS_Programs/React_app/react_demo/src/components/functionalComponent_demo/PersonComponent.js
@@ -8,10 +8,19 @@ const PersonComponent = (props) => {
 
     }
 
+        //state of array type variable
+
+    const [info, setInfo] = useState([501, "MCA","BMS College Of Engineering", "Cricket", "Bangalore", "Karnataka"]);
+
+
 
     return (
-        <div style={{ border: "6px solid blue" }}>HELLO I AM  {props.name} and MY SCORE IS {score}<br />
-            <button style={{height:"35px", width:"100px"}} onClick={changeScore}>Change the score by 4 </button>
+        <div style={{ border: "6px solid green" }}>HELLO I AM  {props.name} and MY SCORE IS {score}<br />
+            <button style={{height:"35px", width:"100px"}} onClick={changeScore}>Change the score by 4 </button><br/><br/>
+
+            INFORMATION = ROLL NO {info[0]}, DEGREE: {info[1]}, COLLEGE: {info[2]}, HOBBY:{info[3]},CITY: {info[4]}, STATE: {info[5]}<br/><br/>
+            <button onClick={() => setInfo([101, ...info])}>CHANGE ROLL NUMBER</button><br/><br/>
+
         </div>
     );
 }
